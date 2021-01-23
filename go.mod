2f71f96e3ce5e67855a048aab7225fa53c2c890d
Ekin <Mac@Ekin-2.local>
2021-01-23 21:54:41 +0300

    go & mongodb crud implementation

--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.12
 
 require (
 	github.com/gorilla/mux v1.7.2
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.4.5
 )
---
+++ helpers/helpers.go
@@ -0,0 +1,85 @@
+package helpers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sample-web-api/models"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Exists(object models.Object, Objects []models.Object) bool {
+	flag := false
+	for _, obj := range Objects {
+		if object.Id == obj.Id {
+			flag = true
+		}
+	}
+
+	return flag
+}
+
+func ConnectDB() *mongo.Collection {
+	config := GetConfiguration()
+	// Set client options
+	clientOptions := options.Client().ApplyURI(config.ConnectionString)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("go-samplewebapi").Collection("go-samplewebapi")
+
+	return collection
+}
+
+type ErrorResponse struct {
+	StatusCode   int    `json:"status"`
+	ErrorMessage string `json:"message"`
+}
+
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
+
+// Configuration model
+type Configuration struct {
+	Port             string
+	ConnectionString string
+}
+
+func GetConfiguration() Configuration {
+	err := godotenv.Load("go.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	configuration := Configuration{
+		os.Getenv("PORT"),
+		os.Getenv("CONNECTION_STRING"),
+	}
+
+	return configuration
+}
